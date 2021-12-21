0f86bd755839eb8d0abcbf817516420a8141e028
Manav <manavdoda7@gmail.com>
2021-12-21 17:15:00 +0530

    Follower following Delete and add functionality is working now

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,76 @@ router.get('/logout', (req, res)=>{
     })
 })
 
+router.get('/removefollower/:username', async (req, res)=>{
+    const followedBy = req.params.username
+    console.log(`GET /user/removefollower/${followedBy} request`);
+    if(req.session.user===undefined) {
+        req.flash('message', 'Please login first.')
+        res.redirect('/login')
+        return
+    }
+    const followed=req.session.user.username
+    try{
+        let check = await User.checkDuplicate(followed, followedBy)
+        check = check[0]
+        if(check.length==0) {
+            req.flash('message', 'This user is not following you.')
+            res.redirect('/home')
+            return
+        }
+    } catch(err) {
+        console.log('Error while checking validity of relationship.', err);
+        req.flash('message', 'Please try again after sometime.')
+        res.redirect('/home')
+        return
+    }
+    try{
+        await User.removeRelation(followed,followedBy)
+    } catch(err) {
+        console.log('Error in removing relationship');
+        req.flash('message', 'Please try again after sometime.', err)
+        res.redirect('/home')
+        return
+    }
+    req.flash('message' ,'Follower removed.')
+    res.redirect('/home')
+})
+
+router.get('/removefollowing/:username', async(req, res)=>{
+    const followed = req.params.username
+    console.log(`GET /user/removefollowing/${followed} request`);
+    if(req.session.user===undefined) {
+        req.flash('message', 'Please login first.')
+        res.redirect('/login')
+        return
+    }
+    const followedBy=req.session.user.username
+    try{
+        let check = await User.checkDuplicate(followed, followedBy)
+        check = check[0]
+        if(check.length==0) {
+            req.flash('message', 'You are not following this user')
+            res.redirect('/home')
+            return
+        }
+    } catch(err) {
+        console.log('Error while checking validity of relationship.', err);
+        req.flash('message', 'Please try again after sometime.')
+        res.redirect('/home')
+        return
+    }
+    try{
+        await User.removeRelation(followed,followedBy)
+    } catch(err) {
+        console.log('Error in removing relationship');
+        req.flash('message', 'Please try again after sometime.', err)
+        res.redirect('/home')
+        return
+    }
+    req.flash('message' ,'Following removed.')
+    res.redirect('/home')
+})
+
 router.post('/follow', async(req, res)=>{
     console.log('POST /user/follow request');
     if(req.session.user===undefined) {
