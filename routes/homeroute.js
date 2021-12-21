96d1c3ef1d7935365a4104b6411b5adc348aea12
Manav <manavdoda7@gmail.com>
2021-12-21 21:52:23 +0530

    Changed home route if authenicated and added start script in package.json file for deployment

--- routes/homeroute.js
+++ routes/homeroute.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 router.get('/', (req, res)=>{
     console.log('GET / request');
+    if(req.session.user!=undefined) res.redirect('/home')
     res.render('index', {message:req.flash('message')})
 })
 
